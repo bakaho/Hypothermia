7cba51adf4d243c6878001784a484c05096270c1
bakaho <cleariqd@gmail.com>
2018-11-29 00:21:23 +0800

    shuffle_btn

--- Hypothermia/Assets/scripts/puzzleController.cs
+++ Hypothermia/Assets/scripts/puzzleController.cs
@@ -5,6 +5,11 @@ using System;
 
 public class puzzleController : MonoBehaviour {
 
+    public GameObject buttonX;
+    public GameObject buttonZ;
+
+    public String theTag = "stadiumPuz";
+
     GameObject[] stdPuzs;
     Vector3[] originalPos = new Vector3[150];
     public float startZpos = -12.75f; //18x
@@ -12,16 +17,20 @@ public class puzzleController : MonoBehaviour {
     public float startXpos = -6.75f; //15x
     public float cubeSize = 1.5f;
     public Vector3 centerPoint = new Vector3(5.25f, -1.2f, 0f);
-    public float boundStartZposL = -8.25f; //18x
-    public float boundStartXposL = -14.25f; //15x
-    public float boundStartZposH = 18.75f; //18x
-    public float boundStartXposH = 14.25f; //15x
+    public float boundStartXposL = -8.25f; //18x
+    public float boundStartZposL = -14.25f; //15x
+    public float boundStartXposH = 18.75f; //18x
+    public float boundStartZposH = 14.25f; //15x
     public int shuffleLB = 2;
     public int shuffleHB = 5;
     public int shuffleTime = 15;
 
     List<GameObject>[] sameX = new List<GameObject>[20];
     List<GameObject>[] sameZ = new List<GameObject>[20];
+
+    List<int> moveZ = new List<int>();
+    List<int> moveX = new List<int>();
+
 
     System.Random rnd = new System.Random();
 
@@ -33,14 +42,24 @@ public class puzzleController : MonoBehaviour {
             sameX[i] = new List<GameObject>();
             sameZ[i] = new List<GameObject>();
         }
-        stdPuzs = GameObject.FindGameObjectsWithTag("stadiumPuz");
+        stdPuzs = GameObject.FindGameObjectsWithTag(theTag);
         for (int i = 0; i < stdPuzs.Length; i++){
             originalPos[i] = new Vector3(stdPuzs[i].transform.position.x,stdPuzs[i].transform.position.y,stdPuzs[i].transform.position.z);
-            //print(originalPos[i].x +" " +originalPos[i].y+" "+originalPos[i].z);
         }
         for (int i = 0; i < shuffleTime;i++){
             shuffle();
             print("shuffle");
+        }
+
+        for (int i = 0; i < moveX.Count; i++){
+            Vector3 spawnPos = new Vector3(-15.75f, -1.5f, startZpos + moveX[i]*cubeSize);
+            Instantiate(buttonX, spawnPos, Quaternion.Euler(0f, 90f, 0f));
+        }
+
+        for (int i = 0; i < moveZ.Count; i++)
+        {
+            Vector3 spawnPos = new Vector3(startXpos + moveZ[i] * cubeSize, -1.5f, -15.75f);
+            Instantiate(buttonZ, spawnPos, Quaternion.Euler(0f, 0f, 0f));
         }
 	}
 	
@@ -86,8 +105,6 @@ public class puzzleController : MonoBehaviour {
         for (int i = 0; i < theStep; i++){
             step(theAxis, theDir, line);
         }
-
-
         
     }
 
@@ -100,7 +117,7 @@ public class puzzleController : MonoBehaviour {
         {//sameZ moveX
             for (int i = 0; i < sameZ[index].Count; i++)
             {
-                print("steps1");
+                
                 Vector3 testPos = new Vector3((float)(sameZ[index][i].transform.position.x + dir * 1.5f), sameZ[index][i].transform.position.y, sameZ[index][i].transform.position.z);
                 if (testPos.x >= boundStartXposH || testPos.x <= boundStartXposL)
                 {
@@ -112,8 +129,12 @@ public class puzzleController : MonoBehaviour {
             if (canMove){
                 for (int i = 0; i < sameZ[index].Count; i++)
                 {
-                    
+                    print("steps1");
                     sameZ[index][i].transform.position = new Vector3((float)(sameZ[index][i].transform.position.x + dir * 1.5f), sameZ[index][i].transform.position.y, sameZ[index][i].transform.position.z);
+                    if (!moveX.Contains(index))
+                    {
+                        moveX.Add(index);
+                    }
                 }
                 
             }
@@ -122,7 +143,7 @@ public class puzzleController : MonoBehaviour {
         {//sameX moveZ
             for (int i = 0; i < sameX[index].Count; i++)
             {
-             print("steps2");
+             
              Vector3 testPos = new Vector3(sameX[index][i].transform.position.x, sameX[index][i].transform.position.y, (float)(sameX[index][i].transform.position.z+ dir * 1.5f));
                 if (testPos.z >= boundStartZposH || testPos.z <= boundStartZposL)
                 {
@@ -135,8 +156,14 @@ public class puzzleController : MonoBehaviour {
             {
                 for (int i = 0; i < sameX[index].Count; i++)
                 {
-                    
+                    print("steps2");
                     sameX[index][i].transform.position = new Vector3(sameX[index][i].transform.position.x, sameX[index][i].transform.position.y, (float)(sameX[index][i].transform.position.z + dir * 1.5f));
+                    if (!moveZ.Contains(index))
+                    {
+                        moveZ.Add(index);
+                    }
+
+
                 }
 
             }
@@ -166,9 +193,5 @@ public class puzzleController : MonoBehaviour {
                 }
             }
         }
-    }
-
-    void checkCollision(){
-        
     }
 }
