19e244e8e4fe5fbb0b6aca17ba66ad2fc6a35ddf
bakaho <cleariqd@gmail.com>
2018-11-28 10:48:27 +0800

    rotation

---
+++ Hypothermia/Assets/scripts/dayNightShift.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dayNightShift : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(new Vector3(10, 0, 0) * Time.deltaTime);
+        if (this.transform.rotation.eulerAngles.x > 170 || this.transform.rotation.x<-30){
+            print("here");
+            this.transform.Rotate(new Vector3(50, 0, 0) * Time.deltaTime);
+        }
+            
+
+	}
+}
