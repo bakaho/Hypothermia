4d54047695f473761c565f11fd0e35a6b455a443
bakaho <cleariqd@gmail.com>
2018-11-28 12:27:47 +0800

    sync_dayNight_snow

--- Hypothermia/Assets/scripts/dayNightShift.cs
+++ Hypothermia/Assets/scripts/dayNightShift.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class dayNightShift : MonoBehaviour {
-	
-	// Update is called once per frame
+
+    // Update is called once per frame
+    //public override void OnStartServer()
+    //{
+    //}
 	void Update () {
         this.transform.Rotate(new Vector3(10, 0, 0) * Time.deltaTime);
         if (this.transform.rotation.eulerAngles.x > 170 || this.transform.rotation.eulerAngles.x<-30){
             print("here");
             this.transform.Rotate(new Vector3(50, 0, 0) * Time.deltaTime);
         }
-            
-
 	}
 }
