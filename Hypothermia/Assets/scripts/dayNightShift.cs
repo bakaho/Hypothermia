7143475bb66e50a34c761285df0aa4439163aa44
bakaho <cleariqd@gmail.com>
2018-11-28 10:21:11 +0800

    dayNightShift

---
+++ Hypothermia/Assets/scripts/dayNightShift.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dayNightShift : MonoBehaviour {
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(50, 0, 0) * Time.deltaTime);
+		
+	}
+}
