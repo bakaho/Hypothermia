316bffee1cf1fda6c1d31549c3bd73d8f1bc0ee3
bakaho <cleariqd@gmail.com>
2018-11-28 17:35:46 +0800

    puz1

--- Hypothermia/Assets/scripts/dayNightShift.cs
+++ Hypothermia/Assets/scripts/dayNightShift.cs
@@ -5,28 +5,30 @@ using UnityEngine.Networking;
 
 public class dayNightShift : MonoBehaviour {
     public bool isNight = false;
+    public float daySpeed = 1;
 
     // Update is called once per frame
     //public override void OnStartServer()
     //{
     //}
 	void Update () {
-        this.transform.Rotate(new Vector3(10, 0, 0) * Time.deltaTime);
+        this.transform.Rotate(new Vector3(daySpeed, 0, 0) * Time.deltaTime);
         if (this.transform.rotation.eulerAngles.x > 170 || this.transform.rotation.eulerAngles.x<-30){
             isNight = true;
             print("here");
-            this.transform.Rotate(new Vector3(50, 0, 0) * Time.deltaTime);
+            this.transform.Rotate(new Vector3(10*daySpeed, 0, 0) * Time.deltaTime);
         }else{
             isNight = false;
         }
         if(isNight){
             if(GetComponent<Light>().intensity > 0){
-                GetComponent<Light>().intensity -= 0.05f;
+                GetComponent<Light>().intensity -= 0.01f;
+                //RenderSettings.ambientLight = Color.black;
             }
         }else{
             if (GetComponent<Light>().intensity < 1f)
             {
-                GetComponent<Light>().intensity += 0.05f;
+                GetComponent<Light>().intensity += 0.01f;
             }
         }
 	}
---
+++ Hypothermia/Assets/scripts/puzzleController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puzzleController : MonoBehaviour {
+
+    GameObject[] stdPuzs;
+    Vector3[] originalPos = new Vector3[150];
+    public float startZpos = -12.75f;
+    public float startYpos = -1.2f;
+    public float startXpos = -6.75f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        stdPuzs = GameObject.FindGameObjectsWithTag("stadiumPuz");
+        for (int i = 0; i < stdPuzs.Length; i++){
+            originalPos[i] = new Vector3(stdPuzs[i].transform.position.x,stdPuzs[i].transform.position.y,stdPuzs[i].transform.position.z);
+            print(originalPos[i].x +" " +originalPos[i].y+" "+originalPos[i].z);
+        }		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
