0cdecd693eb0aa069cafc2bb9c424feb4c9fe12e
bakaho <cleariqd@gmail.com>
2018-11-28 11:07:14 +0800

    snow

--- Hypothermia/Assets/scripts/dayNightShift.cs
+++ Hypothermia/Assets/scripts/dayNightShift.cs
@@ -7,7 +7,7 @@ public class dayNightShift : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         this.transform.Rotate(new Vector3(10, 0, 0) * Time.deltaTime);
-        if (this.transform.rotation.eulerAngles.x > 170 || this.transform.rotation.x<-30){
+        if (this.transform.rotation.eulerAngles.x > 170 || this.transform.rotation.eulerAngles.x<-30){
             print("here");
             this.transform.Rotate(new Vector3(50, 0, 0) * Time.deltaTime);
         }
