c62493b9d61a3049aadea73811d81d95d32d4fb1
bakaho <cleariqd@gmail.com>
2018-11-28 12:50:35 +0800

    light

--- Hypothermia/Assets/scripts/dayNightShift.cs
+++ Hypothermia/Assets/scripts/dayNightShift.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class dayNightShift : MonoBehaviour {
+    public bool isNight = false;
 
     // Update is called once per frame
     //public override void OnStartServer()
@@ -12,8 +13,21 @@ public class dayNightShift : MonoBehaviour {
 	void Update () {
         this.transform.Rotate(new Vector3(10, 0, 0) * Time.deltaTime);
         if (this.transform.rotation.eulerAngles.x > 170 || this.transform.rotation.eulerAngles.x<-30){
+            isNight = true;
             print("here");
             this.transform.Rotate(new Vector3(50, 0, 0) * Time.deltaTime);
+        }else{
+            isNight = false;
+        }
+        if(isNight){
+            if(GetComponent<Light>().intensity > 0){
+                GetComponent<Light>().intensity -= 0.2f;
+            }
+        }else{
+            if (GetComponent<Light>().intensity < 1f)
+            {
+                GetComponent<Light>().intensity += 0.2f;
+            }
         }
 	}
 }
