881c2397e6b4c1a2eb6dfd23a617ee07c60e37b6
bakaho <cleariqd@gmail.com>
2018-11-30 06:50:59 +0800

    dragdrop

---
+++ Hypothermia/Assets/scripts/invDropHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class invDropHandler : MonoBehaviour, IDropHandler {
+    public void OnDrop(PointerEventData eventData)
+    {
+        RectTransform info = transform as RectTransform;
+        if(RectTransformUtility.RectangleContainsScreenPoint(info,Input.mousePosition)){
+            this.gameObject.transform.GetChild(0).GetComponent<Text>().text = "Something from a stadium";
+        }
+    }
+}
