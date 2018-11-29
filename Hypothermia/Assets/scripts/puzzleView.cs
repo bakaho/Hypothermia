3def2687548bc4bf3dc9bedeb584c531aac6e87f
bakaho <cleariqd@gmail.com>
2018-11-29 20:55:44 +0800

    change values

--- Hypothermia/Assets/scripts/puzzleView.cs
+++ Hypothermia/Assets/scripts/puzzleView.cs
@@ -42,51 +42,14 @@ public class puzzleView : MonoBehaviour {
                     transform.Rotate(Vector3.up, rotX);
                     transform.Rotate(Vector3.right, rotY);
                 }
-                //Ray ray = uiCam.ScreenPointToRay(initTouch.position);
-                //RaycastHit hit = new RaycastHit();
-                //if (Physics.Raycast(ray, out hit))
-                //{
-                //    if (hit.transform.gameObject == this.gameObject)
-                //    {
-                //        print("hit!");
-                //        float rotX = xMoved / 5000f * rotSpeed * Mathf.Deg2Rad;
-                //        float rotY = yMoved / 5000f * rotSpeed * Mathf.Deg2Rad;
 
-                //        transform.Rotate(Vector3.up, -rotX);
-                //        transform.Rotate(Vector3.right, rotY);
-                //    }
-                //}
             }
             else if (t.phase == TouchPhase.Ended)
             {
-                //readyDrag = false;
+                rotateAllow = false;
             }
 
 
-
-            //if (Input.GetMouseButtonDown(0))
-            //{
-            //    var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //    var hit : RaycastHit;
-            //    if (Physics.Raycast(ray, hit))
-            //    {
-            //        if (hit.collider.tag == "some_tag")
-            //        {
-            //            // Do something
-            //        }
-            //    }
-            //}
-
         }
     }
-
-	private void OnMouseDrag()
-	{
-        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
-        float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
-
-        transform.Rotate(Vector3.up, -rotX);
-        transform.Rotate(Vector3.right, rotY);
-
-	}
 }
