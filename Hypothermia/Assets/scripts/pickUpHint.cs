ea181687c2433d274f2383d2ef53b93f7bafeee0
bakaho <cleariqd@gmail.com>
2018-11-30 07:27:53 +0800

    update_collision

--- Hypothermia/Assets/scripts/pickUpHint.cs
+++ Hypothermia/Assets/scripts/pickUpHint.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class pickUpHint : MonoBehaviour {
-    public Collider coll;
+    private Collider coll;
 	private void Start()
 	{
         coll = GetComponent<BoxCollider>();
