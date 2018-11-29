2ece47d2e59b54edf80b411300b1b5c4a0cfa8f3
bakaho <cleariqd@gmail.com>
2018-11-29 21:44:02 +0800

    global_touch

--- Hypothermia/Assets/scripts/puzzleView.cs
+++ Hypothermia/Assets/scripts/puzzleView.cs
@@ -8,44 +8,79 @@ public class puzzleView : MonoBehaviour {
     public Camera uiCam;
     bool rotateAllow = false;
     Touch initTouch;
+    Vector3 initMouse;
 
     // Update is called once per frame
     void Update()
     {
-        foreach (Touch t in Input.touches)
+        //foreach (Touch t in Input.touches)
+        //{
+        //    if (t.phase == TouchPhase.Began)
+        //    {
+        //        initTouch = t;
+        //        Ray ray = uiCam.ScreenPointToRay(initTouch.position);
+        //        RaycastHit hit = new RaycastHit();
+        //        if (Physics.Raycast(ray, out hit))
+        //        {
+        //            if (hit.transform.gameObject == this.gameObject)
+        //            {
+        //                print("hit!");
+        //                rotateAllow = true;
+        //            }
+        //        }
+        //    }
+        //    else if (t.phase == TouchPhase.Moved)
+        //    {
+        //        float xMoved = initTouch.position.x - t.position.x;
+        //        float yMoved = initTouch.position.y - t.position.y;
+        //        float distance = Mathf.Sqrt((xMoved * xMoved) + (xMoved * xMoved));
+        //        if(rotateAllow){
+        //            float rotX = xMoved / 5f * rotSpeed * Mathf.Deg2Rad;
+        //            float rotY = yMoved / 5f * rotSpeed * Mathf.Deg2Rad;
+
+        //            transform.Rotate(Vector3.up, rotX);
+        //            transform.Rotate(Vector3.right, rotY);
+        //        }
+
+        //    }
+        //    else if (t.phase == TouchPhase.Ended)
+        //    {
+        //        rotateAllow = false;
+        //    }
+        //}
+
+        if (Input.GetMouseButtonDown(0))
         {
-            if (t.phase == TouchPhase.Began)
+            initMouse = Input.mousePosition;
+            Ray ray = uiCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit))
             {
-                initTouch = t;
-                Ray ray = uiCam.ScreenPointToRay(initTouch.position);
-                RaycastHit hit = new RaycastHit();
-                if (Physics.Raycast(ray, out hit))
+                if (hit.transform.gameObject == this.gameObject)
                 {
-                    if (hit.transform.gameObject == this.gameObject)
-                    {
-                        print("hit!");
-                        rotateAllow = true;
-                    }
+                    print("hit!");
+                    rotateAllow = true;
                 }
             }
-            else if (t.phase == TouchPhase.Moved)
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            float xMoved = Input.mousePosition.x - initMouse.x;
+            float yMoved = Input.mousePosition.y - initMouse.y;
+            float distance = Mathf.Sqrt((xMoved * xMoved) + (xMoved * xMoved));
+            if (rotateAllow)
             {
-                float xMoved = initTouch.position.x - t.position.x;
-                float yMoved = initTouch.position.y - t.position.y;
-                float distance = Mathf.Sqrt((xMoved * xMoved) + (xMoved * xMoved));
-                if(rotateAllow){
-                    float rotX = xMoved / 5f * rotSpeed * Mathf.Deg2Rad;
-                    float rotY = yMoved / 5f * rotSpeed * Mathf.Deg2Rad;
+                float rotX = xMoved / 5f * rotSpeed * Mathf.Deg2Rad;
+                float rotY = yMoved / 5f * rotSpeed * Mathf.Deg2Rad;
 
-                    transform.Rotate(Vector3.up, rotX);
-                    transform.Rotate(Vector3.right, rotY);
-                }
+                transform.Rotate(Vector3.up, -rotX);
+                transform.Rotate(Vector3.right, rotY);
+            }
 
-            }
-            else if (t.phase == TouchPhase.Ended)
-            {
-                rotateAllow = false;
-            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            rotateAllow = false;
         }
     }
 }
