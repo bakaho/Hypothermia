d0ba0ba9c68f2e765c6aefdc7bcf29445a03a12a
bakaho <cleariqd@gmail.com>
2018-11-30 07:21:23 +0800

    click_cube

---
+++ Hypothermia/Assets/scripts/pickUpHint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUpHint : MonoBehaviour {
+    public Collider coll;
+	private void Start()
+	{
+        coll = GetComponent<BoxCollider>();
+	}
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (coll.Raycast(ray, out hit,1000f))
+            {
+                if (hit.transform.gameObject == this.gameObject)
+                {
+                      print("picked!!");
+                      GameObject.FindGameObjectWithTag("frameSlot1").gameObject.transform.GetChild(0).gameObject.SetActive(true);
+
+                }
+            }
+        }
+		
+	}
+}
